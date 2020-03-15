806c60111b7b929d2b007357000b385ee7e7f84e
ItzAkmal <akmal3535.ah@gmail.com>
2020-03-15 19:41:33 +0600

    Animation changed to Bubble Sort, Speed Button

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Bouble Sorting Visualizer
-This a simple sorting visualizer that sorts an array of size 150.
+# Bubble Sorting Visualizer
+
+This a simple Bubble Sorting visualizer that sorts an array.
 
 If you want to test this out , Here is the [Link](http://itzakmal.github.io/sort)
 
--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const BAR_HEIGHT_MIN = 5; //Minimum Height For Bars
 const BAR_HEIGHT_MAX = 640; //Maximum Height For Bars
 const BAR_COLOR = "#00FA9A"; //Color For Normal Bars
-const BAR_SWAPPING_COLOR = "lightcoral"; //Color For Swapping Bars
-let tick_speed = 70; //Initial Tick Speed For Swap
-let bar_width = 4; //Initial Width Of Individual Bars
+const SWAPPED_BAR_COLOR = "lightcoral"; //Color For Swapped Bars
 let max_array_size = 150; //Initial Maximum Size Of Array (Number Of Bars)
+let primary_tick_speed = 10; //Initial Tick Speed For Swap
+let secondary_tick_speed = primary_tick_speed * max_array_size; //Initial Tick Speed For Swap
+let speed = 1; //Initial Sorting Speed
+let bar_width = 4; //Initial Width Of Individual Bars
 let currentBar = max_array_size - 1; //Swap Color Helper
 let previousBar = max_array_size - 1; //Swap Color Helper
 let array = []; //Creates Initial Array
@@ -14,22 +16,31 @@ let array = []; //Creates Initial Array
 const box = document.querySelector(".grid-box");
 const buttonGen = document.querySelector("#btn-gen");
 const buttonSort = document.querySelector("#btn-sort");
+const buttonSpeed = document.querySelector("#btn-speed");
 const buttonReset = document.querySelector("#btn-reset");
 const rangeSlider = document.querySelector("#array-size");
+const rangeLabel = document.querySelector("#array-size-label");
 
-//Initializing Array
+//Initializing Array & Components
 generateArray(max_array_size);
+rangeSlider.value = max_array_size;
+rangeLabel.textContent = `Size: ${max_array_size}`;
+buttonSpeed.disabled = true;
 
 //Adding Event Listeners
 buttonGen.addEventListener("click", () => {
   generateArray(max_array_size);
 });
-buttonSort.addEventListener("click", sortBars);
+buttonSort.addEventListener("click", sortBars2);
+buttonSpeed.addEventListener("click", increaseSpeed);
 buttonReset.addEventListener("click", resetButoon);
 rangeSlider.addEventListener("input", changeArraySize);
 
 //Generates New Array
 function generateArray(maxSize) {
+  //Resets Sorting Speed To 0
+  setSpeedCount(1);
+  speed = 2;
   //Removes the previous array bars
   let child = box.lastElementChild;
   while (child) {
@@ -64,7 +75,6 @@ function randIntInRange(start, end) {
 //Changes Max Array Size
 function changeArraySize() {
   let sliderValue = rangeSlider.value;
-  const rangeLabel = document.querySelector("#array-size-label");
 
   //Changes Max Array Size To The Slider Value
   max_array_size = sliderValue;
@@ -75,16 +85,20 @@ function changeArraySize() {
   //Changes Bar Width And Tick Speed According To Array Size
   if (max_array_size > 100 && max_array_size <= 200) {
     bar_width = 4;
-    tick_speed = 70;
+    primary_tick_speed = 10;
+    secondary_tick_speed = primary_tick_speed * max_array_size;
   } else if (max_array_size <= 100 && max_array_size >= 50) {
     bar_width = 6;
-    tick_speed = 100;
+    primary_tick_speed = 15;
+    secondary_tick_speed = primary_tick_speed * max_array_size;
   } else if (max_array_size < 50) {
     bar_width = 8;
-    tick_speed = 150;
+    primary_tick_speed = 20;
+    secondary_tick_speed = primary_tick_speed * max_array_size;
   } else if (max_array_size > 200) {
     bar_width = 2;
-    tick_speed = 30;
+    primary_tick_speed = 5;
+    secondary_tick_speed = primary_tick_speed * max_array_size;
   }
 
   //Generates New Array For Slider Changes
@@ -92,19 +106,85 @@ function changeArraySize() {
 }
 
 //Changes The Bar Height In DOM (Iterates)
-function sortBars() {
-  let i;
+// function sortBars() {
+//   let i;
+//   buttonGen.disabled = true;
+//   buttonSort.disabled = true;
+//   rangeSlider.disabled = true;
+//   for (i = 0; i < array.length; i++) {
+//     setTimeout(() => {
+//       sortArray();
+//     }, i * primary_tick_speed);
+//   }
+//   //Resets Bar Color Positions
+//   currentBar = max_array_size - 1;
+//   previousBar = max_array_size - 1;
+// }
+
+//Bouble Sorting Animation
+function sortBars2() {
+  //Disables/Enables Buttons When Sorting
   buttonGen.disabled = true;
   buttonSort.disabled = true;
   rangeSlider.disabled = true;
-  for (i = 0; i < array.length; i++) {
+  buttonSpeed.disabled = false;
+
+  bubbleSort(array);
+  const bars = document.querySelectorAll(".grid-bar");
+  let lastBar = array.length - 1;
+
+  for (let k = 0; k < array.length; k++) {
     setTimeout(() => {
-      sortArray();
-    }, i * tick_speed);
+      for (let i = 1; i < array.length; i++) {
+        setTimeout(() => {
+          let currentHeight = deletePX(bars[i - 1].style.height);
+          let nextHeight = deletePX(bars[i].style.height);
+
+          //Sorts Bars
+          if (currentHeight > nextHeight) {
+            bars[i - 1].style.height = `${nextHeight}px`;
+            bars[i].style.height = `${currentHeight}px`;
+          }
+
+          //Changes Color Of Sorted Arrays
+          if (lastBar >= 0) {
+            let lastHeight = deletePX(bars[lastBar].style.height);
+            if (lastHeight === array[lastBar]) {
+              bars[lastBar].style.backgroundColor = SWAPPED_BAR_COLOR;
+              lastBar--;
+            }
+          }
+        }, i * primary_tick_speed);
+      }
+    }, k * secondary_tick_speed);
   }
+
   //Resets Bar Color Positions
   currentBar = max_array_size - 1;
   previousBar = max_array_size - 1;
+}
+
+//Deletes px From Height Value
+function deletePX(str) {
+  let str2 = str.split("");
+  str2.pop();
+  str2.pop();
+  return parseInt(str2.join(""));
+}
+
+//Increases Sorting Speed
+function increaseSpeed() {
+  if (speed <= 10) {
+    //Changes Speed Multiplier
+    setSpeedCount(speed++);
+
+    sortBars2();
+  }
+}
+
+//Sets Speed Counter Text
+function setSpeedCount(num) {
+  buttonSpeed.textContent = `x${num} Speed`;
 }
 
 //Resets Disabled Buttons
@@ -112,41 +192,57 @@ function resetButoon() {
   buttonGen.disabled = false;
   buttonSort.disabled = false;
   rangeSlider.disabled = false;
+  buttonSpeed.disabled = true;
 }
 
 //Changes Bar Height In DOM (Single)
-function sortArray() {
-  const bars = document.querySelectorAll(".grid-bar");
-  boubleSort(array);
+// function sortArray() {
+//   const bars = document.querySelectorAll(".grid-bar");
+//   bubbleSort(array);
 
-  for (let i = 0, j = 1; i < array.length; i++, j++) {
-    bars[i].style.height = `${array[i]}px`;
-  }
+//   for (let i = 0, j = 1; i < array.length; i++, j++) {
+//     bars[i].style.height = `${array[i]}px`;
+//   }
 
-  //Changes Swapping Bar Color To Red And Green
-  bars[currentBar].style.backgroundColor = BAR_SWAPPING_COLOR;
-  if (previousBar > currentBar) {
-    bars[previousBar].style.backgroundColor = BAR_COLOR;
-    previousBar--;
-  }
-  currentBar--;
-}
+//   //Changes Swapping Bar Color To Red And Green
+//   bars[currentBar].style.backgroundColor = SWAPPED_BAR_COLOR;
+//   if (previousBar > currentBar) {
+//     bars[previousBar].style.backgroundColor = BAR_COLOR;
+//     previousBar--;
+//   }
+//   currentBar--;
+// }
 
 //Sorts The Given Array Using Bouble Sort
-function boubleSort(arr) {
-  let swapped = true;
+// function bubbleSort(arr) {
+//   let swapped = true;
 
-  while (swapped) {
-    for (let i = 0; i < arr.length; i++) {
-      swapped = false;
-      if (arr[i] > arr[i + 1]) {
-        let temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
+//   while (swapped) {
+//     for (let i = 0; i < arr.length; i++) {
+//       swapped = false;
+//       if (arr[i] > arr[i + 1]) {
+//         let temp = arr[i];
+//         arr[i] = arr[i + 1];
+//         arr[i + 1] = temp;
+//         swapped = true;
+//       }
+//     }
+//   }
+
+//   console.log(arr);
+//   return arr;
+// }
+
+function bubbleSort(arr) {
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
       }
     }
   }
-
   return arr;
 }
